32e61407266f59a278d3f93e9bdcac4d29b5defb
Michaël Cloots <michael.cloots@thomasmore.be>
2025-10-19 23:25:48 +0200

    http post

--- src/app/signup-validator/signup-validator.ts
+++ src/app/signup-validator/signup-validator.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ForbiddenNames } from '../forbidden-names';
 import { SignupDto } from '../signup-dto';
+import { SignupService } from '../signup-service';
 
 @Component({
   selector: 'app-signup-validator',
@@ -11,10 +12,22 @@ import { SignupDto } from '../signup-dto';
 })
 export class SignupValidator {
   model: SignupDto = { name: '', email: '', password: '' };
+  private signupService = inject(SignupService);
 
   onSubmit(form: NgForm) {
     if (form.valid) {
       console.log('submit', this.model);
+      this.signupService.postSignup(this.model).subscribe({
+        next: res => {
+          // Do something on success
+          if (res.success) {
+            console.log(res);
+          }
+        },
+        error: err => {
+          console.error(err);
+        }
+      });
       form.resetForm();
     }
   }
