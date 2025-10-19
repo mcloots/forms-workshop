9d5584d6aba3ded2b9dd2ecca6ad9553fd838623
Michaël Cloots <michael.cloots@thomasmore.be>
2025-10-19 21:05:28 +0200

    address dynamic lines ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Signup } from './signup/signup';
+import { Address } from './address/address';
 
 export const routes: Routes = [
-    { path: "signup", component: Signup }
+    { path: "signup", component: Signup },
+    { path: "dynamic", component: Address }
 ];
